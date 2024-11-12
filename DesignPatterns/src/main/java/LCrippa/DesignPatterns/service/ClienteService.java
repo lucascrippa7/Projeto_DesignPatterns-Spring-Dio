12d3c27bd00302bb98f50b95dbe163264068abac
Lucas <lucas_crippa@hotmail.com>
2024-11-12 01:19:50 -0300

    Dio-Projeto

---
+++ DesignPatterns/src/main/java/LCrippa/DesignPatterns/service/ClienteService.java
@@ -0,0 +1,19 @@
+package LCrippa.DesignPatterns.service;
+
+
+import LCrippa.DesignPatterns.model.Cliente;
+
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
